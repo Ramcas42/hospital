7ef3ff3ad5a8f31a1d2ce53f95c4ab269855dccf
David Lancashire <david@popupchinese.com>
2019-08-06 03:08:36 +0900

    simplification of module core

--- hospital.js
+++ hospital.js
@@ -9,12 +9,8 @@ function Hospital(app) {
 
   this.app             = app;
 
-  this.tasks           = {};
-
   this.name            = "Hospital";
   this.browser_active  = 0;
-  this.handlesEmail    = 1;
-  this.emailAppName    = "Hospital";
 
   return this;
 }
@@ -22,33 +18,20 @@ function Hospital(app) {
 module.exports = Hospital;
 util.inherits(Hospital, ModTemplate);
 
-Hospital.prototype.onConfirmation = function onConfirmation(blk, tx, conf, app) {
-    if (tx.transaction.msg.module != "Hospital") { return; }
-    if (conf == 0) {
-      hospital = app.modules.returnModule("hospital");
-      switch (tx.transaction.msg.type) {
-        case "task":
-          this.addTask(tx);
-        case "checkbox":
-          this.toggleCheckbox(tx);
-        default:
-          break;
-      }
-    }
-  }
 
-  Hospital.prototype.createHospitalTX = function createHospitalTx(data) {
-    var newtx = this.app.wallet.createUnsignedTransactionWithDefaultFee(this.app.wallet.returnPublicKey());
-  
-    newtx.transaction.msg = Object.assign({}, data, { module: "Hospital" });
-  
-    var newtx = this.app.wallet.signTransaction(newtx);
-  
-    this.app.network.propagateTransactionWithCallback(newtx, () => {
-      if (this.app.BROWSER) {
-        alert("your message was propagated")
-      }
-    });
-  
-    return newtx;
+
+
+
+Hospital.prototype.onConfirmation = function onConfirmation(blk, tx, conf, app) {
+
+  if (tx.transaction.msg.module != "Hospital") { return; }
+
+  if (conf == 0) {
+
+    let hospital = app.modules.returnModule("hospital");
+
   }
+}
+
+
+
